514709de77dc3bde00181182b2de44a629ce0148
GUODONGTAN <gdtan021115@gmail.com>
2023-06-08 15:44:01 +0800

    docstrings added

--- RowColumnTranspositionCipher.py
+++ RowColumnTranspositionCipher.py
@@ -4,7 +4,11 @@ import math
 class RowColumnTranspositionCipher(object):
     
     def __init__ (self, key):
-        # The key should contains only alphabet A-Z and without space.
+        """Intialize the key of the cipher.
+
+        Args:
+            key (_str_): The keyword which consists of uppercase letters (A-Z) only
+        """
         self.key = key.upper()
         
     # Helper function
@@ -14,9 +18,11 @@ class RowColumnTranspositionCipher(object):
     #     matrix[row_b] = temp   
         
     def encrypt(self, message):
-        """
+        """ The method is used to encrypt the message.
+        
         Pseudocode: 
-        1) Construct the matrix.
+        1) Construct an empty matrix with size [m, n], where m = number of rows, n = number of columns.
+            The number of columns is determined by the length of the keyword.
         2) Compute the key order and swapping list. 
            The swapping list contains the index of rows that need to be swapped.
         3) Write the message and padding characters into the matrix row-wise.
@@ -24,6 +30,12 @@ class RowColumnTranspositionCipher(object):
            i th element in the swapping list is swapped with the row with the index of (i+1)th element in the
            swapping list, starting from the first to the (key.length-1)th element.
         5) Read the ciphertext column-wise based on the key order.
+
+        Args:
+            message (_str_): the message to be encrypted
+
+        Returns:
+            _str_: the encrypted message
         """
         cipher = ""
         
@@ -89,16 +101,28 @@ class RowColumnTranspositionCipher(object):
     def decrypt(self, cipher):
         """
         Pseudocode:
-        1) Construct the transpose matrix.
+        1) Construct the empty transpose matrix with size [n, m].
+        2) Compute the key order and swapping list. 
+        3) Write the ciphertext into the matrix constructed row-wise according to the key order. Then,
+            transpose the matrix to get the decipher matrix.
+        4) Perform ERO where two rows are swapped based on the swapping list in reverse order.
+        5) Read the plaintext starting from the first to the last row of the matrix.
         """
         message = ""
         
         key_length = len(self.key)
         msg_length = len(cipher)
         
+        # Compute an empty transpose matrix
         column_size = key_length
         row_size = math.ceil(msg_length / key_length)
         
+        transpose = []
+        for col in range(column_size):
+            transpose += [[None] * row_size]
+        
+        # Compute the key order and swapping list. The swapping list is in reverse order to the swapping
+        # list in the encrypt method.
         key_list = list((ord(key[i]) % 65) for i in range(key_length))
         key_order = [0 for i in range(key_length)]
         key_dict = {}
@@ -115,10 +139,8 @@ class RowColumnTranspositionCipher(object):
         
         swapping = np.array(list([key_list[i], key_list[i-1]] for i in range(key_length-1, 0, -1))) % row_size
         
-        transpose = []
-        for col in range(column_size):
-            transpose += [[None] * row_size]
-            
+  
+        # Write the ciphertext into the matrix constructed row-wise according to the key order
         index = 0
         for c in range(column_size):
            current = key_order.index(c)
@@ -126,6 +148,7 @@ class RowColumnTranspositionCipher(object):
                transpose[current][r] = cipher[index]
                index += 1 
             
+        # Transpose the matrix to get the decipher matrix
         transpose = np.array(transpose)
         decipher_matrix = transpose.transpose().tolist()
         
@@ -136,6 +159,7 @@ class RowColumnTranspositionCipher(object):
         for r in range(row_size):
             message += ''.join(decipher_matrix[r])
         
+        # Remove the padding in the message
         padding_count = message.count('_')
         message = message[:-padding_count]
         
